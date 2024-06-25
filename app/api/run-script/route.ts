aed34d809a2209b2c7a2fa36f46460bba7830a0a
Piotr Maciejewski <pether.maciejewski@gmail.com>
2024-06-25 10:55:37 +0200

    vercel update

--- app/api/run-script/route.ts
+++ app/api/run-script/route.ts
@@ -1,73 +1,39 @@
 import { NextRequest } from "next/server";
 import { RunEventType, RunOpts } from "@gptscript-ai/gptscript";
-import g from "../../../lib/gptScriptInstance";
-import { join } from "path";
-import { execFile } from "child_process";
-import { promisify } from "util";
-import fs from "fs";
-
-const execFileAsync = promisify(execFile);
+import g from "@/lib/gptScriptInstance";
 
 const script = "app/api/run-script/story-book.gpt";
 
-// Determine the path to the gptscript binary dynamically
-const gptScriptPath = join(
-  process.cwd(),
-  "node_modules",
-  "@gptscript-ai",
-  "gptscript",
-  "bin",
-  "gptscript"
-);
-
-export const maxDuration = 60; // Set this as needed
+export const maxDuration = 30;
 
 export async function POST(request: NextRequest) {
   const { story, pages, path } = await request.json();
+  console.log("Streaming started", script);
+  // Example CLI command: gptscript ./story-book.gpt --story "A robot and human become friends" --pages 5 --path ./
 
   const opts: RunOpts = {
     disableCache: true,
-    input: `--story ${story} --pages ${pages} --path ${path}` ?? "",
+    input: `--story ${story} --pages ${pages} --path ${path}`,
   };
 
   try {
-    if (!fs.existsSync(gptScriptPath)) {
-      throw new Error(`gptscript binary not found at ${gptScriptPath}`);
-    }
-
     const encoder = new TextEncoder();
     const stream = new ReadableStream({
       async start(controller) {
-        const timeoutId = setTimeout(() => {
-          controller.error(new Error("Script execution timed out"));
-          console.error("Error: Script execution timed out");
-        }, maxDuration * 1000);
-
         try {
-          const { stdout, stderr } = await execFileAsync(gptScriptPath, [
-            script,
-            //@ts-ignore
-            ...opts.input.split(" "),
-          ]);
+          const run = await g.run(script, opts);
 
-          if (stderr) {
-            throw new Error(stderr);
-          }
-
-          const run = JSON.parse(stdout);
-
-          run.on(RunEventType.Event, (data: any) => {
+          run.on(RunEventType.Event, (data) => {
             controller.enqueue(
               encoder.encode(`event: ${JSON.stringify(data)}\n\n`)
             );
           });
 
+          await run.text();
           controller.close();
         } catch (error: any) {
           controller.error(error);
           console.error("Error:", error);
-        } finally {
-          clearTimeout(timeoutId);
         }
       },
     });
